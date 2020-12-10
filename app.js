2530028bf3e7b8043a5662c997a6eca7537dbfdf
Francisco Martins <fmaraposo@gmail.com>
2020-12-10 15:50:55 +0000

    deployment final version 3

--- app.js
+++ app.js
@@ -36,12 +36,7 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+
 
 app.use(express.static(path.join(__dirname, 'dist')));
 /* app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico'))); */
