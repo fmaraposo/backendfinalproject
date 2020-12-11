20b242d4642ddbc7aaf6b90ea7c2651e232cd1d9
Francisco Martins <fmaraposo@gmail.com>
2020-12-11 10:50:12 +0000

    allows cors

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.CLIENT_HOSTNAME], //where the requests come from, is the url of the frontend. The Backend must allow FrontEnd.
+    origin: ['https://dahost.herokuapp.com'], //where the requests come from, is the url of the frontend. The Backend must allow FrontEnd.
   })
 );
 
